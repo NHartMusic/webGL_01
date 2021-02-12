3413c5afdd02ce88261374c7b4097e50d61ef1b1
Nick Hart <nhartmusic@gmail.com>
2021-02-12 14:21:18 +0100

    draggin a dang box around

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+let angle = 0
+
 function setup() {
-    createCanvas(400, 400, WEBGL)
+    createCanvas(400, 300, WEBGL)
 }
 
 function draw() {
-    background(51)
+    background(175)
 
-    fill(255, 0, 150)
-    rect(0, 0, 50, 50)
+    rectMode(CENTER)
+    noStroke()
+    fill(0, 0, 255)
+    translate(mouseX - width/2, mouseY - height/2)
+    rotateX(angle)
+    rotateY(angle * 0.3)
+    rotateZ(angle * 1.2)
+
+    
+    box(10, 100, 50)
+
+    angle+=0.03
 }
