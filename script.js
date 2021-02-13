03492be8a86e02dc698c40764d1df30392115f4a
Nick Hart <nhartmusic@gmail.com>
2021-02-13 15:14:10 +0100

    panning camera back and forth

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 let angle = 0
 
+
 function setup() {
-    createCanvas(displayWidth, displayHeight, WEBGL)
+    createCanvas(windowWidth, windowHeight, WEBGL)
 }
 
 function draw() {
-    let dx = mouseX - width / 2;
-    let dy = mouseY - height / 2;
-    let v = createVector(dx,dy,0);
-    v.normalize();
-    
-    ambientLight(0, 0, 255)
-    directionalLight(255, 0, 0, v)
-    
-    background(175)
-    specularMaterial(0, 0, 255)
-    noStroke()
+    background(0)
+
+    let cameraX = map(mouseX, 0, width, 200, -200)
+    camera(cameraX, 0, (height/2) / tan(PI/6), cameraX, 0, 0, 0, 1, 0)
+    ambientLight(255)
+
+    push()
     rotateX(angle)
     rotateY(angle * 0.5)
- 
+    noStroke()
+    normalMaterial()
+    box(100)
+    
+    pop()
 
-    torus(100, 25)
-
+    translate(0, 100)
+    
+    rotateX(HALF_PI)
+    noStroke()
+    ambientMaterial(255)
+    plane(500,500)
     angle+=0.03
 }
