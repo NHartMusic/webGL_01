de44ee6e92b056dbcd5d60ad7f1fdaa943533ac0
Nick Hart <nhartmusic@gmail.com>
2021-02-14 17:06:16 +0100

    3d text floating around

--- script.js
+++ script.js
@@ -14,30 +14,33 @@ function preload() {
 
 function setup() {
     createCanvas(400, 400, WEBGL)
-    graphics = createGraphics(200, 200)
-    graphics.background(255)
+    //graphics = createGraphics(300, 300)
+    //graphics.background(255)
 
-    love = createGraphics(200, 200)
-    love.background(255, 100)
+    love = createGraphics(300, 300)
+    //love.background(255, 100)
     love.fill(255)
+    noStroke()
     love.textAlign(CENTER)
     love.textSize(64)
-    love.text('love', 100, 100)
+    love.text('love', 150, 150)
 }
 
 function draw() {
     background(0)
 
-    graphics.fill(255, 0, 255)
-    graphics.ellipse(mouseX, mouseY, 20)
+    // graphics.fill(255, 0, 255)
+    // graphics.ellipse(mouseX, mouseY, 20)
     ambientLight(255)
-    directionalLight(255, 255, 255, 1, 0, 1)
+    // directionalLight(255, 255, 255, 1, 0, 1)
     rotateX(angle)
     rotateY(angle * 1.1)
     rotateZ(angle * 0.8)
 
     texture(love)
-    box(150)
+
+    plane(300, 300)
+
 
     angle+=0.02
 }
